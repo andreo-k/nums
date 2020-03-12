78a71ce7ff13eccdc2b084c84363e9fb8376aa88
andrey.kozhevnikov <444221@gmail.com>
2020-03-12 16:28:14 +0500

    initial project setup

---
+++ src/main/java/andrey/co/App.java
@@ -0,0 +1,13 @@
+package andrey.co;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
